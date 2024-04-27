67449d2fd3ef8a32d1e11ef807e90b572c868b75
Vikash Sharma <82925579+vikash28-cloud@users.noreply.github.com>
2024-04-27 16:00:51 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import './index.css'
 import Navbar from "./components/Navbar"
 import Manager from './components/Manager'
 import Footer from './components/Footer'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './components/Login';
+import Signup from './components/Signup';
 function App() {
   return <>
+ <BrowserRouter>
  <Navbar/>
- {/* <div className='min-h-[87vh]'> */}
- <Manager/>
+      <Routes>
+       <Route path='/login' element={<Login />}/>
+       <Route path='/' element={<Manager />}/>
+       <Route path='/signup' element={<Signup />}/>
+      
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
  
- {/* </div> */}
- <Footer/>
   </>
 }
 
